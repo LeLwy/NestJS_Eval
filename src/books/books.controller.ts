dfd7c11b3fb49dbee5c1019f614d418cddfd4373
LeLwy <Louis.Chancioux@gmail.com>
2024-10-22 11:53:50 +0200

    Modifications du controller de books

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateBookDto } from './dto/CreateBook.dto';
 
 @Controller('books')
 export class BooksController {}
+
+@Controller('user')
+export class UserController {
+
+    BooksService : BooksService
+
+    constructor() {
+        this.BooksService = new BooksService
+    }
+
+    @Get('/:id')
+    getUser(@Param('id') id: string){
+        return this.BooksService.findOne(id);
+    }
+
+    @Get()
+    listUsers(){
+        return this.BooksService.findAll();
+    }
+
+    @Post()
+    createUser(@Body() body: CreateBookDto){
+        return this.BooksService.create(body.content)
+    }
+}
