f3abdab23f8e03f2c5783e517cf523abfb730081
LeLwy <Louis.Chancioux@gmail.com>
2024-10-22 12:03:36 +0200

    Modifs sur users.controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/CreateUser.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    UsersService : UsersService
+
+    constructor() {
+        this.UsersService = new UsersService
+    }
+
+    @Get('/:id')
+    getUser(@Param('id') id: string){
+        return this.UsersService.findOne(id);
+    }
+
+    @Get()
+    listUsers(){
+        return this.UsersService.findAll();
+    }
+
+    @Post()
+    createUser(@Body() body: CreateUserDto){
+        return this.UsersService.create(body.content)
+    }
+}
