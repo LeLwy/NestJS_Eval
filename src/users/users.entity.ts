e319f0f6091d3ec1e9522de1b789524287b58fee
LeLwy <Louis.Chancioux@gmail.com>
2024-10-22 12:16:02 +0200

    Implémentation de users.entity

---
+++ src/users/users.entity.ts
@@ -0,0 +1,21 @@
+import { AfterInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  firstname: string;
+
+  @Column()
+  lastname: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('User created with id ' + this.id);
+  }
+}
